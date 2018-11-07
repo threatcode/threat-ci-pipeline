09b3517c86315e3b3549b1ff0f1fcf527615516f
Agustin Henze <tin@aayy.com.ar>
2018-11-07 21:08:02 +0100

    Remove already updated repositories

--- repositories.py
+++ repositories.py
@@ -1,7 +1,4 @@
 REPOS = [
-    'debian/aiocoap.git',
-    'debian/gcc-arm-none-eabi.git',
-    'debian/python-cbor.git',
     'debian/python-stem.git',
     'python-team/modules/python-fisx',
     'samba-team/samba',
