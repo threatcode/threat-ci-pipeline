c9ad4278bfff9cee5f275744f159ba050a34e756
Agustin Henze <tin@aayy.com.ar>
2018-11-10 19:40:27 +0100

    Remove deprecated workaround for gitlab include
    We are not going to miss this piece of sh^Wcode

--- repositories.py
+++
@@ -1,8 +0,0 @@
-REPOS = [
-    'debian/python-stem.git',
-    'python-team/modules/python-fisx',
-    'samba-team/samba',
-    'science-team/pyfai.git',
-    'science-team/pymca.git',
-    'science-team/silx.git',
-]
