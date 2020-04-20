3f944a194b516bd5084bbf76d44a1ebdf202ce41
Iñaki Malerba <inaki@malerba.space>
2020-04-20 12:41:51 +0200

    Generate build dependencies package on TMPDIR.
    Avoid leaving extra files on PWD by moving the destination path for
    mk-build-deps to a different directory.

--- images/scripts/install-build-deps.sh
+++ images/scripts/install-build-deps.sh
@@ -4,12 +4,14 @@
 # Copying and distribution of this file, with or without modification, are
 # permitted in any medium without royalty provided the copyright notice and
 # this notice are preserved. This file is offered as-is, without any warranty.
+export TMPDIR=/tmp/build-deps
+mkdir -p $TMPDIR
 
 echo " -> Starting installing the build-deps"
 
 /usr/local/bin/mk-ci-build-deps debian/control
 
-FILENAME=$(echo *-build-deps*.deb)
+FILENAME=$(echo ${TMPDIR}/*-build-deps*.deb)
 PKGNAME=$(dpkg-deb -f ${FILENAME} Package)
 
 dpkg --force-depends --force-conflicts -i $FILENAME
@@ -23,8 +25,6 @@ aptitude -y --without-recommends \
 	-o "Aptitude::ProblemResolver::Keep-All-Level=55000" \
 	-o "Aptitude::ProblemResolver::Remove-Essential-Level=maximum" \
 	install $PKGNAME
-
-rm $FILENAME
 
 if ! dpkg -l $PKGNAME 2>/dev/null | grep -q ^ii; then
 	echo "Aptitude couldn't satisfy the build dependencies"
